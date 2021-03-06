d430a3423e3db155547b52b5bf9c7f76ed33ee4c
ken505 <68226398+ken505@users.noreply.github.com>
2021-03-07 06:37:44 +0900

    wip:

--- src/components/InlineStyle.jsx
+++ src/components/InlineStyle.jsx
@@ -1,8 +1,21 @@
 export const InlineStyle = () => {
+  const containerStyle = {
+    color: "gray",
+    backgroundColor: "lavenderblush",
+    boxShadow: "8px 8px 8px gainsboro",
+    borderRadius: "4px",
+    padding: "20px",
+    margin: "20px",
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center"
+  };
+  const titleStyle = {};
+  const buttonStyle = {};
   return (
-    <div>
-      <p>- What are you looking at -</p>
-      <button> Fuck off </button>
+    <div style={containerStyle}>
+      <p style={titleStyle}>- What are you looking at -</p>
+      <button style={buttonStyle}> Fuck off </button>
     </div>
   );
 };
